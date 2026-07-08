0691a00031ede8d8e5ceface259611a47758bee2
zaryab02-eng <mohdzaryab99@gmail.com>
2026-07-08 13:40:32 +0530

    solving

--- dsaJS/01.js
+++ dsaJS/01.js
@@ -1,13 +1,30 @@
-function largestMultipleOf3(num) {
-  let largestMultipleOf3 = null;
+// function largestMultipleOf3(num) {
+//   let largestMultipleOf3 = null;
+//   for (let i = 0; i < num.length; i++) {
+//     if (num[i] % 3 === 0 && num[i] > largestMultipleOf3) {
+//       largestMultipleOf3 = num[i];
+//     }
+//   }
+//   if (largestMultipleOf3 === null) {
+//     return -1;
+//   }
+//   return largestMultipleOf3;
+// }
+// console.log(largestMultipleOf3([5, 9, 12, 7, 18, 10]));
+
+function smallestMultipleOf5(num) {
+  let smallestMultiple = null;
   for (let i = 0; i < num.length; i++) {
-    if (num[i] % 3 === 0 && num[i] > largestMultipleOf3) {
-      largestMultipleOf3 = num[i];
+    if (num[i] % 5 === 0) {
+      if (smallestMultiple === null) {
+        smallestMultiple = num[i];
+      } else if (num[i] < smallestMultiple) {
+        smallestMultiple = num[i];
+      }
     }
   }
-  if (largestMultipleOf3 === null) {
-    return -1;
-  }
-  return largestMultipleOf3;
+  if (smallestMultiple === null) return -1;
+  return smallestMultiple;
 }
-console.log(largestMultipleOf3([5, 9, 12, 7, 18, 10]));
+
+console.log(smallestMultipleOf5([12, 25, 7, 5, 30, 18]));
